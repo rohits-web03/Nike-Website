275c90edf5811adc71d623a21de0fee87fcd7ef5
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-24 07:06:21 +0530

    Update Hero.jsx

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Button from '../Components/Button'
 import { arrowRight } from '../assets/icons'
 import { shoes, statistics } from '../Constants'
@@ -6,6 +7,7 @@ import { bigShoe1 } from '../assets/images'
 import ShoeCard from '../Components/ShoeCard'
 
 const Hero = () => {
+  const [largeShoeImg, setlargeShoeImg] = useState(bigShoe1);
   return (
     <section 
       id='home'
@@ -13,14 +15,14 @@ const Hero = () => {
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span
           className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
-        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-          Discover stylish Nike arrivals,quality comfort,
+        <p className='font-montserrat text-slate-gray text-xl leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort,
           and innovation for your active life.
         </p>
         <Button 
@@ -39,23 +41,22 @@ const Hero = () => {
             })}
         </div>
       </div>
-      <div className=''>
-          <img src={bigShoe1}
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+          <img src={largeShoeImg}
             alt='Shoe Collection'
             width={610}
             height={500}
             className='object-contain relative z-10'
           />
 
-          <div>
+          <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
             {shoes.map((shoe)=>(
-              <div key={shoe.bigShoe}>
                 <ShoeCard 
+                  key={shoe.bigShoe}
                   imgURL={shoe}
-                  changeLargeShoeImg={()=>{}}
-                  largeShoeImg=""
+                  changeLargeShoeImg={(shoe)=>setlargeShoeImg(shoe)}
+                  largeShoeImg={largeShoeImg}
                 />
-              </div>
             ))}
           </div>
       </div>

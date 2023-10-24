973eec8f986c755dd3e8144d9401ad1057532e01
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-24 23:07:42 +0530

    Update Service.jsx

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -3,11 +3,14 @@ import {ServiceCard} from "../Components"
 
 const Services = () => {
   return (
+    <>
+    <h2 className="text-4xl p-4 mb-[2rem] font-bold font-palanquin">Our<span className="text-coral-red"> Services</span></h2>
     <section className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service)=>(
         <ServiceCard key={service.label} {...service} />
       ))}
     </section>
+    </>
   )
 }
 

63188877aab8bb8cd678279fcf3b68cf8ebf36f5
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-24 23:09:01 +0530

    Adding App.jsx,main.jsx and index.css

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {Hero,PopularProducts,Services,Subscribe,SpecialOffer,SuperQuality,CustomerReview,Footer} from "./Sections/index";
+import Navbar from "./Components/Navbar";
+
+const App=()=>(
+    <main className="relative">
+      <Navbar />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section id="services" className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReview />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+
+);
+
+export default App;

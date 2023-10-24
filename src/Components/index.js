80e00ec502decd54092afde7fb4c7b6f187cd8b7
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-24 23:07:06 +0530

    Adding the remaining Components

---
+++ src/Components/index.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Button from "./Button";
+import PopularProductsCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ShoeCard from "./ShoeCard";
+
+export {
+    Navbar,
+    Button,
+    PopularProductsCard,
+    ServiceCard,ShoeCard
+}
